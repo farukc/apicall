b2eb0722ba0558ea7ddaee0094950ab2b0dd3879
Faruk Celik <farukc@microsoft.com>
2017-02-25 21:21:10 +0200

    Create DELETE.php

---
+++ PHP/DELETE.php
@@ -0,0 +1,36 @@
+<?php
+// This sample uses the Apache HTTP client from HTTP Components (http://hc.apache.org/httpcomponents-client-ga/)
+require_once 'HTTP/Request2.php';
+
+$request = new Http_Request2('https://foo.bar/buzz');
+$url = $request->getUrl();
+
+$headers = array(
+    // Request headers
+    'Ocp-Apim-Subscription-Key' => '{subscription key}',
+);
+
+$request->setHeader($headers);
+
+$parameters = array(
+    // Request parameters
+);
+
+$url->setQueryVariables($parameters);
+
+$request->setMethod(HTTP_Request2::METHOD_DELETE);
+
+// Request body
+$request->setBody("{body}");
+
+try
+{
+    $response = $request->send();
+    echo $response->getBody();
+}
+catch (HttpException $ex)
+{
+    echo $ex;
+}
+
+?>
