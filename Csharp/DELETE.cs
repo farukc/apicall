d7517b99122fec2fa52b5606c91989883ae4dc6b
Faruk Celik <farukc@microsoft.com>
2017-02-25 21:08:46 +0200

    Create DELETE.cs

---
+++ Csharp/DELETE.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Net.Http;
+using System.Web;
+
+namespace CSHttpClientSample
+{
+    static class Program
+    {
+        static void Main()
+        {
+            MakeRequest();
+            Console.WriteLine("Hit ENTER to exit...");
+            Console.ReadLine();
+        }
+        
+        static async void MakeRequest()
+        {
+            var client = new HttpClient();
+            var queryString = HttpUtility.ParseQueryString(string.Empty);
+
+            // Request headers
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", "{subscription key}");
+
+            var uri = "https://foo.bar/buzz?" + queryString;
+
+            var response = await client.DeleteAsync(uri);
+        }
+    }
+}	
