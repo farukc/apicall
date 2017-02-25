f02de9ce73802c2a1a0f2a1d130e5e0181707aed
Faruk Celik <farukc@microsoft.com>
2017-02-25 21:02:36 +0200

    Update GET.bat

--- CURL/GET.bat
+++ CURL/GET.bat
@@ -1,6 +1,6 @@
+@ECHO OFF
 
-curl -v -X POST "https://foo.bar/buzz"
--H "Content-Type: application/json"
+curl -v -X GET "https://fcbounworkshop.azure-api.net/echo/resource?param1=sample&param2={number}"
 -H "Ocp-Apim-Subscription-Key: {subscription key}"
 
 --data-ascii "{body}" 
