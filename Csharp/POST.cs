4077ddcfdf080480b11a68df9ae1b75683fe2d27
Faruk Celik <farukc@microsoft.com>
2017-02-25 21:06:23 +0200

    Create POST.cs

---
+++ Csharp/POST.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Net.Http;
+using System.Web;
+
+namespace CSHttpClientSample
+{
+    static class Program
+    {
+        static void Main()
+        {
+            MakeRequest();
+            Console.WriteLine("Hit ENTER to exit...");
+            Console.ReadLine();
+        }
+        
+        static async void MakeRequest()
+        {
+            var client = new HttpClient();
+            var queryString = HttpUtility.ParseQueryString(string.Empty);
+
+            // Request headers
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", "{subscription key}");
+
+            var uri = "https://foo.bar/buzz?" + queryString;
+
+            HttpResponseMessage response;
+
+            // Request body
+            byte[] byteData = Encoding.UTF8.GetBytes("{body}");
+
+            using (var content = new ByteArrayContent(byteData))
+            {
+               content.Headers.ContentType = new MediaTypeHeaderValue("< your content type, i.e. application/json >");
+               response = await client.PostAsync(uri, content);
+            }
+
+        }
+    }
+}	
